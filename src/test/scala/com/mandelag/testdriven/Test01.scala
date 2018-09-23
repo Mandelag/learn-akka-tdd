4d5e849948b6f3eee738d280287b3ffe90837c50
Keenan Gebze <keenan.gebze@gmail.com>
2018-09-23 21:02:03 +0700

    Fixing the wrong placement of the CenterPoint test.

--- src/test/scala/com/mandelag/testdriven/Test01.scala
+++ src/test/scala/com/mandelag/testdriven/Test01.scala
@@ -23,14 +23,13 @@ class Test01 extends TestKit(ActorSystem("test-system"))
             distanceCalculator ! CalculateDistance(Coordinate(4,0), Coordinate(0,3))
             expectMsg(5)
         }
-    }
-    
-    "Return a point between two points correctly, within certain epsilon" in {
-        val EPSILON = 0.001
-        
-        val distanceCalculator = TestActorRef[DistanceCalculator]
-        
-        distanceCalculator ! CenterPoint(Coordinate(100, 50), Coordinate(-100, -50))
-        expectMsg(0)
+        "Return a point between two points correctly, within certain epsilon" in {
+            val EPSILON = 0.001
+            
+            val distanceCalculator = TestActorRef[DistanceCalculator]
+            
+            val centerPoint = distanceCalculator ! CenterPoint(Coordinate(100, 50), Coordinate(-100, -50))
+            assert(centerPoint.equals(Coordinate(0,0)))
+        }
     }
 }
