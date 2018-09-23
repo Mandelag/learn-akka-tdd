ffb6b963fe7296688b4286eed57a0577af25434b
Keenan Gebze <keenan.gebze@gmail.com>
2018-09-23 21:22:37 +0700

    Override equals method within Coordinate case class.

--- src/main/scala/com/mandelag/testdriven/DistanceCalculator.scala
+++ src/main/scala/com/mandelag/testdriven/DistanceCalculator.scala
@@ -8,7 +8,7 @@ object DistanceCalculator {
             val EPSILON: Double = 0.001
             other match {
                 case Coordinate(x, y) => {
-                    (this.x - x) <  EPSILON && (this.y - y) < EPSILON
+                    Math.abs(this.x - x) <  EPSILON && Math.abs(this.y - y) < EPSILON
                 }
                 case _ => false
             }
@@ -24,11 +24,18 @@ class DistanceCalculator extends Actor {
     
     def receive = {
         case CalculateDistance(c1,c2) => {
-           sender() ! calculateDistance(c1,c2)
+           sender() ! calculateDistance(c1, c2)
+        }
+        case CenterPoint(c1,c2) => {
+            sender() ! centerPoint(c1, c2)
         }
     }
     
     private def calculateDistance(c1: Coordinate, c2: Coordinate): Double = {
         100.0
+    }
+    
+    private def centerPoint(c1: Coordinate, c2: Coordinate): Coordinate = {
+        Coordinate(0, 0)
     }
 }
