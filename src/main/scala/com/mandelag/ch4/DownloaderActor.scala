259b7106e851d5b14f7b1fd46c1bff2b413f2636
Keenan Gebze <keenan.gebze@gmail.com>
2018-10-11 00:25:13 +0700

    Learning to implement akka-http in the program..

--- src/main/scala/com/mandelag/ch4/DownloaderActor.scala
+++ src/main/scala/com/mandelag/ch4/DownloaderActor.scala
@@ -1,6 +1,10 @@
 package com.mandelag.ch4
 
 import akka.actor._
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 object DownloaderActor {
     def props(url: String) = Props(new DownloaderActor(url))
@@ -8,12 +12,22 @@ object DownloaderActor {
     case class GetData()
     case class DownloadedData(str: String)
 }
+
 class DownloaderActor(url: String) extends Actor {
     import DownloaderActor._
+    import akka.pattern.pipe
+    import context.dispatcher
+    
+    val http = Http(context.system)
+
     def receive =  {
         case Fetch() => sender() ! url
-        case GetData() => sender() ! getData
+        case GetData() => {
+            http.singleRequest(HttpRequest(uri=url)).pipeTo(self)
+            sender() ! ""
+        }
+        case HttpResponse(StatusCodes.OK, headers, entity, _) => {
+            sender() ! headers
+        }
     }
-
-    private def getData = ???
 }
